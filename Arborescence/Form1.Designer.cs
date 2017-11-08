b428e3fe845d4e1b11c5a56c7456b83ca7102e6e
amerlier <salut-yah@yahoo.fr>
2017-11-08 10:48:57 +0100

    ArboPhase2Finished

--- Arborescence/Form1.Designer.cs
+++ Arborescence/Form1.Designer.cs
@@ -29,37 +29,34 @@
         private void InitializeComponent()
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
-            this.buttonHD = new System.Windows.Forms.Button();
             this.buttonExpansion = new System.Windows.Forms.Button();
             this.buttonReduction = new System.Windows.Forms.Button();
             this.textBoxWay = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonParcourir = new System.Windows.Forms.Button();
+            this.buttonExplore = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxSelec = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxSelec.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(89, 75);
+            this.treeView1.Location = new System.Drawing.Point(12, 61);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(466, 260);
+            this.treeView1.Size = new System.Drawing.Size(437, 375);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
-            // buttonHD
-            // 
-            this.buttonHD.Location = new System.Drawing.Point(130, 12);
-            this.buttonHD.Name = "buttonHD";
-            this.buttonHD.Size = new System.Drawing.Size(90, 57);
-            this.buttonHD.TabIndex = 1;
-            this.buttonHD.Text = "Affichage du disque dur";
-            this.buttonHD.UseVisualStyleBackColor = true;
-            this.buttonHD.Click += new System.EventHandler(this.buttonHD_Click);
-            // 
             // buttonExpansion
             // 
-            this.buttonExpansion.Location = new System.Drawing.Point(130, 341);
+            this.buttonExpansion.Location = new System.Drawing.Point(12, 442);
             this.buttonExpansion.Name = "buttonExpansion";
             this.buttonExpansion.Size = new System.Drawing.Size(90, 57);
             this.buttonExpansion.TabIndex = 2;
@@ -69,7 +66,7 @@
             // 
             // buttonReduction
             // 
-            this.buttonReduction.Location = new System.Drawing.Point(413, 341);
+            this.buttonReduction.Location = new System.Drawing.Point(108, 442);
             this.buttonReduction.Name = "buttonReduction";
             this.buttonReduction.Size = new System.Drawing.Size(90, 57);
             this.buttonReduction.TabIndex = 3;
@@ -79,27 +76,18 @@
             // 
             // textBoxWay
             // 
-            this.textBoxWay.Location = new System.Drawing.Point(455, 28);
+            this.textBoxWay.Location = new System.Drawing.Point(12, 12);
             this.textBoxWay.Name = "textBoxWay";
-            this.textBoxWay.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWay.Size = new System.Drawing.Size(437, 20);
             this.textBoxWay.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(302, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Chemin du dossier à examiner";
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 395);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 508);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(648, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1002, 22);
             this.statusStrip1.TabIndex = 6;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -109,23 +97,85 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
+            // buttonParcourir
+            // 
+            this.buttonParcourir.Location = new System.Drawing.Point(12, 32);
+            this.buttonParcourir.Name = "buttonParcourir";
+            this.buttonParcourir.Size = new System.Drawing.Size(75, 23);
+            this.buttonParcourir.TabIndex = 7;
+            this.buttonParcourir.Text = "Parcourir";
+            this.buttonParcourir.UseVisualStyleBackColor = true;
+            this.buttonParcourir.Click += new System.EventHandler(this.buttonHD_Click);
+            // 
+            // buttonExplore
+            // 
+            this.buttonExplore.Location = new System.Drawing.Point(374, 32);
+            this.buttonExplore.Name = "buttonExplore";
+            this.buttonExplore.Size = new System.Drawing.Size(75, 23);
+            this.buttonExplore.TabIndex = 8;
+            this.buttonExplore.Text = "Explorer";
+            this.buttonExplore.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Enabled = false;
+            this.textBoxLog.Location = new System.Drawing.Point(6, 19);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(523, 162);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxLog);
+            this.groupBox1.Location = new System.Drawing.Point(455, 249);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(535, 187);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // groupBoxSelec
+            // 
+            this.groupBoxSelec.Controls.Add(this.listBox1);
+            this.groupBoxSelec.Location = new System.Drawing.Point(455, 61);
+            this.groupBoxSelec.Name = "groupBoxSelec";
+            this.groupBoxSelec.Size = new System.Drawing.Size(535, 182);
+            this.groupBoxSelec.TabIndex = 11;
+            this.groupBoxSelec.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(523, 160);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 417);
+            this.ClientSize = new System.Drawing.Size(1002, 530);
+            this.Controls.Add(this.groupBoxSelec);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonExplore);
+            this.Controls.Add(this.buttonParcourir);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxWay);
             this.Controls.Add(this.buttonReduction);
             this.Controls.Add(this.buttonExpansion);
-            this.Controls.Add(this.buttonHD);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBoxSelec.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,13 +184,18 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button buttonHD;
         private System.Windows.Forms.Button buttonExpansion;
         private System.Windows.Forms.Button buttonReduction;
         private System.Windows.Forms.TextBox textBoxWay;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonParcourir;
+        private System.Windows.Forms.Button buttonExplore;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxSelec;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Arborescence/Form1.cs
+++ Arborescence/Form1.cs
@@ -17,41 +17,85 @@ namespace Arborescence
         {
             InitializeComponent();
         }
-
+        DirectoryInfo directoryInfo;
         private void buttonHD_Click(object sender, EventArgs e)
         {
-            DirectoryInfo directoryInfo = new DirectoryInfo(textBoxWay.Text);
-            bool state = false;
-            if (directoryInfo.Exists)
+            textBoxLog.Text = DateTime.Now.ToLongTimeString() + " Log started";
+            try
             {
-                state = true;
-                TreeNode rootNode = new TreeNode(directoryInfo.Name);
+                folderBrowserDialog1.ShowDialog();
+                textBoxWay.Text = folderBrowserDialog1.SelectedPath;
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " Début scan " + folderBrowserDialog1.SelectedPath);
+                directoryInfo = new DirectoryInfo(textBoxWay.Text);
+                bool state = false;
+                if (directoryInfo.Exists)
+                {
+                    state = true;
+                    TreeNode rootNode = new TreeNode(directoryInfo.Name);
 
-                        treeView1.Nodes.Add(rootNode);
-                        GetDirectories(directoryInfo.GetDirectories(), rootNode);
-                        toolStripStatusLabel1.Text = "Exploration du dossier terminée : " + treeView1.GetNodeCount(state) + " objets trouvés";
-                        buttonExpansion.Enabled = true;
-                        buttonReduction.Enabled = true;
+                    treeView1.Nodes.Add(rootNode);
+                    GetDirectories(directoryInfo.GetDirectories(), rootNode);
+                    toolStripStatusLabel1.Text = "Exploration du dossier terminée : " + treeView1.GetNodeCount(state) + " objet(s) trouvé(s)";
+                    buttonExpansion.Enabled = true;
+                    buttonReduction.Enabled = true;
+                    textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " Fin Scan " + folderBrowserDialog1.SelectedPath);
+
+                }
+                else
+                    MessageBox.Show("Le répertoire saisi n'existe pas");
             }
-            else
-                MessageBox.Show("Le répertoire saisi n'existe pas");
+            catch (DirectoryNotFoundException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + "Dossier introuvable" + _e.ToString());
+            }
+            catch(AccessViolationException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " L'accès au chemin d'accès " + directoryInfo.Name + " est refusé" + _e.ToString());
+            }
+            catch(UnauthorizedAccessException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " L'accès au chemin d'accès " + directoryInfo.Name + " est refusé" + _e.ToString());
+            }
+            catch (Exception _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " Erreur" + _e.ToString());
+            }
         }
         private void GetDirectories(DirectoryInfo[] directoryInfo, TreeNode node)
         {
-            DirectoryInfo[] directory;
-            TreeNode treeNode;
-            foreach (DirectoryInfo subdir in directoryInfo)
+            try
             {
-                directory = subdir.GetDirectories();
-                treeNode = new TreeNode(subdir.Name);
-                node.Nodes.Add(treeNode);
-                GetDirectories(directory, treeNode);
+                DirectoryInfo[] directory;
+                TreeNode treeNode;
+                foreach (DirectoryInfo subdir in directoryInfo)
+                {
+                    directory = subdir.GetDirectories();
+                    treeNode = new TreeNode(subdir.Name);
+                    node.Nodes.Add(treeNode);
+                    GetDirectories(directory, treeNode);
+                }
+            }
+            catch (DirectoryNotFoundException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + "Dossier introuvable" + _e.ToString());
+            }
+            catch (AccessViolationException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " L'accès au chemin d'accès " + folderBrowserDialog1.SelectedPath + " est refusé" + _e.ToString());
+            }
+            catch (UnauthorizedAccessException _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " L'accès au chemin d'accès " + folderBrowserDialog1.SelectedPath + " est refusé" + _e.ToString());
+            }
+            catch (Exception _e)
+            {
+                textBoxLog.AppendText("\r\n" + DateTime.Now.ToLongTimeString() + " Erreur" + _e.ToString());
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBoxWay.Text = @"c:\inetpub";
+            textBoxWay.Text = string.Empty;
             buttonReduction.Enabled = false;
             buttonExpansion.Enabled = false;
         }
@@ -59,6 +103,15 @@ namespace Arborescence
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
             ActiveForm.Text = "Vous avez sélectionné : " + treeView1.SelectedNode.Text;
+            groupBoxSelec.Text = "Sélection : " + folderBrowserDialog1.SelectedPath + @"\" + treeView1.SelectedNode.Text;
+            FileInfo[] dirs = new FileInfo[Directory.GetFiles(directoryInfo.FullName).Length];
+
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                dirs = directoryInfo.GetFiles();
+                listBox1.Items.Add(dirs[i]);
+            }
+
         }
 
         private void buttonExpansion_Click(object sender, EventArgs e)
@@ -69,6 +122,12 @@ namespace Arborescence
         private void buttonReduction_Click(object sender, EventArgs e)
         {
                 treeView1.CollapseAll();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo open = new System.Diagnostics.ProcessStartInfo(folderBrowserDialog1.SelectedPath + @"\" + listBox1.SelectedItem.ToString());
+            System.Diagnostics.Process.Start(open);
         }
     }
 }
